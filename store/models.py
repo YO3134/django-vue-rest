d696318a4a949583328e836ab342d89e6b5f369a
YO3134 <nishiura.145@gmail.com>
2020-12-21 23:47:47 +0900

    Add class Book to store/models.py

--- store/models.py
+++ store/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Book(models.Model):
+    class Meta:
+        db_table = "book"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(ver_bose="タイトル", max_length=20)
+    price = models.IntegerField(ver_bose="価格", null=True)
+    created_at = models.DataTimeField(defautl=timezone)
+
+    def __str__(self):
+        return self.title
