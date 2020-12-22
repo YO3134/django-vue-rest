ae9d7fffa2eeddc28cfca776b8b50e6776f7a947
YO3134 <nishiura.145@gmail.com>
2020-12-22 23:46:59 +0900

    Add class BookModelAdmin to store/models.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'id', 'created_at')
+    ordering = ('-created_at')
+
+admin.site.register(Book, BookModelAdmin)
 # Register your models here.
