579b386f79fa103045477ba1cb50a4c1be4d4b5a
YO3134 <nishiura.145@gmail.com>
2020-12-24 23:14:58 +0900

    Add class BookViewSet to apibackend/views.py

--- apibackend/views.py
+++ apibackend/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from store.models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.object.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 # Create your views here.
