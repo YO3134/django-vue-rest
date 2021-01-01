32fd22a603b14f0a3f3868895999b2277804cbca
YO3134 <nishiura.145@gmail.com>
2021-01-01 23:10:28 +0900

    Add import App,router,store new.Vue to src/main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+BootstrapVue.setConfig.productionTip = process.env.NODE_ENV === 'production'
+Vue.use(BootstrapVue)
 
 createApp(App).mount('#app')
+
+new.Vue({
+    router,
+    store,
+    render: h =>h(App),
+}).mount('#app')
