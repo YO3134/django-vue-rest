2a355f45edcdf0508b29c709561c6d405cb1898d
YO3134 <nishiura.145@gmail.com>
2021-01-15 23:43:15 +0900

    Add methods:submitSave to front/src/pages/HomePage.vue

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -27,3 +27,48 @@
         </main>
     </div>
 </template>
+<script>
+import api from '@/service/api'
+import GlobalHeader from '@/components/GlobalHeader.vue'
+import GlobalMessage from '@/components/GlobalMessage.vue'
+
+export default {
+    components: {
+        GlobalHeader,
+        GlobalMessage
+    },
+    data () {
+        return {
+            from: {
+                book: {
+                    title: '',
+                    price: 0
+                }
+            }
+        }
+    },
+    computed: {
+        isCreated: function () {
+            return this.from.book.id !== undefined
+        }
+    },
+    methods: {
+        submitSave: function () {
+            api({
+                method: this.isCreated ? 'put' : 'post',
+                url: this.isCreated ? '/books/' + this.from.book.id + '/' : '/books/',
+                data: {
+                    'id': this.from.id,
+                    'title': this.from.book.title,
+                    'price': this.from.book.price
+                }
+            })
+            .then(response => {
+                const message = this.isCreated ? '更新しました。' : '登録しました。'
+                this.$store.dispatch('message/setInfoMessage', { message : message })
+                this.from.book = response.data
+            })
+        }
+    }
+}
+</script>
