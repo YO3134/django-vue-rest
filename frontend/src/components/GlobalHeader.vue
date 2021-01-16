0896ba5ccaf4c3a649ec2d51fd9a31ca8446be99
YO3134 <nishiura.145@gmail.com>
2021-01-16 22:54:58 +0900

    Create frontend/src/components/GlobalHeader.vue
    Add template to components/GlobalHeader.vue

---
+++ frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="header">
+        <b-navbar type = "dark" variant = "dark">
+            <a href="/" class="navbar-brand">DRF Sample Test</a>
+            <b-navbar-nav class="mi-auto" v-if="$route.meta.requiresAuth">
+                <b-nav-item-dropdown right v-if="isLoggedIn">
+                    <template slot="button-content">{{ username }}</template>
+                    <b-dropdown-item href = "#" @click="clickLogout">ログアウト</b-dropdown-item>
+                </b-nav-item-dropdown>
+                <b-nav-item href = "#" @click="clickLogin">ログイン</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
